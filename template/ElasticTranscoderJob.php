2c165a714c98ce34a9d15b9a07f5ba572cbb4aac
chens3 <chens3@tcnj.edu>
2015-03-08 16:44:37 -0400

    'Added ElasticTranscoderJob.php'

---
+++ template/ElasticTranscoderJob.php
@@ -0,0 +1,60 @@
+<?php
+// Perform Elastic Transcoding Jobs
+// REQUIRES:
+//     1. AWS Credentials 
+//     2. S3 Input/Output buckets already set up (must be on same account as Credentials)
+//     3. Corresponding PipelineID set up and provided
+	
+include 'vendor/autoload.php';
+use Aws\ElasticTranscoder\ElasticTranscoderClient;	
+	
+class ElasticTranscoderJob
+{
+	private $input; //input file name 
+	private $output; //output file name 
+	private $folder; //user folder for input/output
+	
+	//constructor - performs conversion
+	public function __construct($input, $output, $folder)
+	{
+		$this->input = $input;
+		$this->output = $output;
+		$this->folder = $folder;
+		
+		$client = ElasticTranscoderClient::factory(array(
+			'key' => 'AKIAI35YL7GQGKUUAWYA',
+			'secret' => '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',
+			'region'  => 'us-west-2'
+		));
+		
+		$result = $client->createJob(array(
+			// PipelineId is required
+			'PipelineId' => '1425787425117-1042wm',
+			'Input' => array(
+				'Key' => $input,
+				'FrameRate' => 'auto',
+				'Resolution' => 'auto',
+				'AspectRatio' => 'auto',
+				'Interlaced' => 'auto',
+				'Container' => 'auto',
+			),
+			'Output' => array(
+				'Key' => $output,
+				'ThumbnailPattern' => '',
+				'Rotate' => 'auto',
+				'PresetId' => '1351620000001-300010' //320kb-s MP3 - CHANGE THIS
+			),
+			'OutputKeyPrefix' => $folder.'/'
+		));
+		
+		echo "<br>";
+		echo "FILE CONVERTED. "; //prints if conversion was successful
+		echo "<br>";
+	}
+	
+	public function __toString()
+	{
+		return "NULL";
+	}
+}
+?>
