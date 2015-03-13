94aa7f96a936a01a825ef1539b891d6e1f0481e0
Hardik Patel <patelh4@tcnj.edu>
2015-03-13 01:13:29 -0400

    represents a chat user

---
+++ permanent_users_list/chatuser.php
@@ -0,0 +1,13 @@
+<?php
+	class chatuser
+	{
+		public $username;
+		public $roomid;
+		
+		public function __construct($username, $roomid)
+		{
+			$this->username = $username;
+			$this->roomid = $roomid;
+		}
+	}
+?>
