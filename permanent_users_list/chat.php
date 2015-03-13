d5cc04b8f16fff0e98f90b8d807c251fb237b3d4
Hardik Patel <patelh4@tcnj.edu>
2015-03-13 01:08:16 -0400

    Create chat.php
    did not change anything yet

---
+++ permanent_users_list/chat.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8' />
+<style type="text/css">
+<!--
+.chat_wrapper {
+	width: 500px;
+	margin-right: auto;
+	margin-left: auto;
+	background: #CCCCCC;
+	border: 1px solid #999999;
+	padding: 10px;
+	font: 12px 'lucida grande',tahoma,verdana,arial,sans-serif;
+}
+.chat_wrapper .message_box {
+	background: #FFFFFF;
+	height: 150px;
+	overflow: auto;
+	padding: 10px;
+	border: 1px solid #999999;
+}
+.chat_wrapper .panel input{
+	padding: 2px 2px 2px 5px;
+}
+.system_msg{color: #BDBDBD;font-style: italic;}
+.user_name{font-weight:bold;}
+.user_message{color: #88B6E0;}
+-->
+</style>
+</head>
+<body>	
+<?php 
+$colours = array('007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00');
+$user_colour = array_rand($colours);
+?>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<script language="javascript" type="text/javascript">  
+$(document).ready(function(){
+    
+    var divRID = getQueryVariable("id");
+    document.getElementById("rid").innerHTML = "<center>Room ID: " + divRID + "</center>";
+
+	//create a new WebSocket object.
+	var wsUri = "ws://45.56.97.42/server.php"; 	
+	websocket = new WebSocket(wsUri); 
+	
+	websocket.onopen = function(ev) { // connection is open 
+		$('#message_box').append("<div class=\"system_msg\">Connected!</div>"); //notify user
+        //
+		//GET LIST OF USERS IN THE SAME ROOM FROM SERVER
+	}
+
+	$('#send-btn').click(function(){ //use clicks message send button	
+		var mymessage = $('#message').val(); //get message text
+		var myname = $('#name').val(); //get user name
+        var roomID = getQueryVariable("id");
+		
+		if(myname == ""){ //empty name?
+			alert("Enter your name please!");
+			return;
+		}
+		if(mymessage == ""){ //emtpy message?
+			alert("Enter Some message Please!");
+			return;
+		}
+		
+		//prepare json data
+		var msg = {
+		message: mymessage,
+		name: myname,
+		color : '<?php echo $colours[$user_colour]; ?>',
+        room : roomID
+		};
+		//convert and send data to server
+		websocket.send(JSON.stringify(msg));
+	});
+	
+	//#### Message received from server?
+	websocket.onmessage = function(ev) {
+		var msg = JSON.parse(ev.data); //PHP sends Json data
+		var type = msg.type; //message type
+		var umsg = msg.message; //message text
+		var uname = msg.name; //user name
+		var ucolor = msg.color; //color
+        var uroomid = msg.room; // room id
+
+        var gatherID = getQueryVariable("id");
+        if(uroomid === gatherID)
+        {
+            if(type == 'usermsg') 
+            {
+                $('#message_box').append("<div><span class=\"user_name\" style=\"color:#"+ucolor+"\">"+uname+"(" + uroomid +")"+"</span> : <span class=\"user_message\">"+umsg+"</span></div>");
+            }
+        }
+		if(type == 'system')
+		{
+			$('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
+		}
+		
+		$('#message').val(''); //reset text
+	};
+	
+	websocket.onerror	= function(ev){$('#message_box').append("<div class=\"system_error\">Error Occurred - "+ev.data+"</div>");}; 
+	websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");}; 
+});
+    
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("?");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+</script>
+<div id="rid"></div>
+<div class="chat_wrapper">
+<div class="message_box" id="message_box"></div>
+<div class="panel">
+<input type="text" name="name" id="name" placeholder="Your Name" maxlength="10" style="width:20%"  />
+<input type="text" name="message" id="message" placeholder="Message" maxlength="80" style="width:60%" />
+<button id="send-btn">Send</button>
+</div>
+</div>
+<div id="users"></div>
+
+</body>
+</html>
