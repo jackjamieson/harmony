886a144adfcaeb82c78500f7938e8a5d0ab3cf47
chens3 <chens3@tcnj.edu>
2015-03-25 18:24:56 -0400

    Integrated chat feature with room.php (connects to Hardik's chat server)

--- room.php
+++ room.php
@@ -83,7 +83,127 @@
                 <h3 class="panel-title">Chat</h3>
               </div>
               <div class="panel-body">
-                Chatroom could go here probably.
+                <?php 
+				$colours = array('007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00');
+				$user_colour = array_rand($colours);
+				?>
+
+				<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+				<script> 
+					var users = []; // global array
+				</script>
+
+				<script language="javascript" type="text/javascript">  
+				$(document).ready(function(){
+
+					var divRID = getQueryVariable("id");
+					document.getElementById("rid").innerHTML = "<center>Room ID: " + divRID + "</center>";
+
+					//create a new WebSocket object.
+					var wsUri = "ws://45.56.97.42:9000/server.php"; 	
+					websocket = new WebSocket(wsUri); 
+					
+					websocket.onopen = function(ev) { // connection is open 
+						$('#message_box').append("<div class=\"system_msg\">Connected!</div>"); //notify user
+						$('#user_list').append("Connected Users:");
+					}
+
+					$('#send-btn').click(function(){ //use clicks message send button	
+						var mymessage = $('#message').val(); //get message text
+						var myname = $('#name').val(); //get user name
+						var roomID = getQueryVariable("id");
+						
+						if(myname == ""){ //empty name?
+							alert("Enter your name please!");
+							return;
+						}
+						if(mymessage == ""){ //emtpy message?
+							alert("Enter Some message Please!");
+							return;
+						}
+						
+						//prepare json data
+						var msg = {
+						message: mymessage,
+						name: myname,
+						color : '<?php echo $colours[$user_colour]; ?>',
+						room : roomID
+						};
+						//convert and send data to server
+						websocket.send(JSON.stringify(msg));
+					});
+					
+					//#### Message received from server?
+					websocket.onmessage = function(ev) {
+						var msg = JSON.parse(ev.data); //PHP sends Json data
+						var type = msg.type; //message type
+						var umsg = msg.message; //message text
+						var uname = msg.name; //user name
+						var ucolor = msg.color; //color
+						var uroomid = msg.room; // room id
+						var foundMatch = "NO";
+
+						var gatherID = getQueryVariable("id");
+						if(uroomid === gatherID)
+						{
+							if(type == 'usermsg') 
+							{
+								$('#message_box').append("<div><span class=\"user_name\" style=\"color:#"+ucolor+"\">"+uname+"(" + uroomid +")"+"</span> : <span class=\"user_message\">"+umsg+"</span></div>");
+
+								//This loops will find a username match within the user array
+								for(i = 0; i < users.length + 1; i++)
+								{
+									if(users[i] == uname)
+									{
+										foundMatch = "YES";
+									}
+								}
+								//document.getElementById("user_list").innerHTML = users.toString() + "yo"; //debugging
+								
+								//if not match is found, then display the user name only once	
+								if(foundMatch == "NO")
+								{
+									users.push(uname); //push a new user into the array
+									$('#user_list').append("<div><span class=\"user_name\" style=\"color:#"+ucolor+"\">"+uname+"</span></div>");
+								}
+							}
+						}
+						if(type == 'system')
+						{
+							$('#message_box').append("<div class=\"system_msg\">"+umsg+"</div>");
+						}
+						
+						$('#message').val(''); //reset text
+					};
+					
+					websocket.onerror	= function(ev){$('#message_box').append("<div class=\"system_error\">Error Occurred - "+ev.data+"</div>");}; 
+					websocket.onclose 	= function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");}; 
+				});
+					
+				function getQueryVariable(variable)
+				{
+					   var query = window.location.search.substring(1);
+					   var vars = query.split("?");
+					   for (var i=0;i<vars.length;i++) {
+							   var pair = vars[i].split("=");
+							   if(pair[0] == variable){return pair[1];}
+					   }
+					   return(false);
+				}
+				</script>
+				<div id="rid"></div>
+				<div class="chat_wrapper">
+				<div class="message_box" id="message_box"></div>
+				<div class="panel">
+				<input type="text" name="name" id="name" placeholder="Name" maxlength="10" style="width:20%"  />
+				<input type="text" name="message" id="message" placeholder="Message" maxlength="80" style="width:60%" />
+				<button id="send-btn">Send</button>
+				</div>
+				</div>
+				<div id="users"></div>
+
+				<div class = "chat_wrapper2">
+				<div class = "user_list" id = "user_list"></div>
               </div>
             </div> 
              
