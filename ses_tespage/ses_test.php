17c0472c0a3dc7ab16f264027fa2770de9844db4
Hardik Patel <patelh4@tcnj.edu>
2015-04-06 19:20:02 -0400

    Works with multiple, user chosen email address

--- ses_tespage/ses_test.php
+++ ses_tespage/ses_test.php
@@ -19,42 +19,50 @@
 
 	echo "Ses Client created.\n";
 
-	$address = $_GET["address"];
-
-	$result = $client->sendEmail(array(
-    // Source is required
-    'Source' => 'harmony.mailservice@gmail.com',
-    // Destination is required
-    'Destination' => array(
-        'ToAddresses' => array($address),
-        //'CcAddresses' => array('string', ... ),
-        //'BccAddresses' => array('string', ... ),
-    ),
-    // Message is required
-    'Message' => array(
-        // Subject is required
-        'Subject' => array(
-            // Data is required
-            'Data' => 'whats up. its yo boi harmony'
-            //'Charset' => 'string',
+	/*
+    Loops for each email address the user entered
+    need to replace the email text data with an official link
+    Link should be of the form: http://45.56.101.195/room.php?id=' . $id . '
+    Need to integrate this code with create.php
+    */
+    for($i=0;$i<count($_POST["emailAddress"]);$i++)
+    {
+        //$toAddress = $_POST["emailAddress"][$i]
+    	$result = $client->sendEmail(array(
+        // Source is required
+        'Source' => 'harmony.mailservice@gmail.com',
+        // Destination is required
+        'Destination' => array(
+            'ToAddresses' => array($_POST["emailAddress"][$i]),
+            //'CcAddresses' => array('string', ... ),
+            //'BccAddresses' => array('string', ... ),
         ),
-        // Body is required
-        'Body' => array(
-            'Text' => array(
+        // Message is required
+        'Message' => array(
+            // Subject is required
+            'Subject' => array(
                 // Data is required
-                'Data' => 'hows my stream? holla back atchyo boi'
+                'Data' => 'whats up. its yo boi harmony'
                 //'Charset' => 'string',
-            )/*
-            'Html' => array(
-                // Data is required
-                'Data' => 'string',
-                'Charset' => 'string',
-            ),*/
-        ),
-    ),/*
-    'ReplyToAddresses' => array('string', ... ),
-    'ReturnPath' => 'string',*/
-	));
+            ),
+            // Body is required
+            'Body' => array(
+                'Text' => array(
+                    // Data is required
+                    'Data' => 'hows my stream? holla back atchyo boi'
+                    //'Charset' => 'string',
+                )/*
+                'Html' => array(
+                    // Data is required
+                    'Data' => 'string',
+                    'Charset' => 'string',
+                ),*/
+            ),
+        ),/*
+        'ReplyToAddresses' => array('string', ... ),
+        'ReturnPath' => 'string',*/
+    	));
+    }
 
 	echo "Email sent\n";
 ?>
