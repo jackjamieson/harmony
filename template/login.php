c64af426f9ef2bebb1c3260a23dddad19f3b5dda
root <root@ip-172-31-23-42.ec2.internal>
2015-04-08 22:38:12 +0000

    Added login page

---
+++ template/login.php
@@ -0,0 +1,33 @@
+<?php
+include "base.php";
+
+//Checking login on form submit
+if(!empty($_POST['uLogin']) && !empty($_POST['uPassword']))
+{	
+	//First retrieves the hashed password from the database.
+	$passQuery = $conn->prepare("SELECT user_id, password, email, karma FROM user WHERE username=?");
+
+	$passQuery->bind_param("s", $_POST['uLogin']);
+	$passQuery->execute();
+	$result= $passQuery->get_result();
+
+	$row = $result->fetch_row();
+	$hash = $row[1];
+
+	if(password_verify($_POST['uPassword'], $hash))
+	{	
+		$_SESSION['Username'] = $_POST['uLogin'];
+		$_SESSION['User_id'] = $row[0];
+		$_SESSION['EmailAddress'] = $row[2];
+		$_SESSION['Karma'] = $row[3];
+		$_SESSION['LoggedIn'] = 1;
+		
+		unset($_POST['uPassword']);
+		header('Location: manage.php');
+		die();
+	}	
+}
+
+header('Location: index.php');
+die();
+?>
