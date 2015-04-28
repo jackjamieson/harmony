6810334a409cb9113dbb9e6ec53088da23eb75c1
Hardik Patel <patelh4@tcnj.edu>
2015-04-28 18:31:48 -0400

    added in user and pass in the email

--- register.php
+++ register.php
@@ -86,7 +86,7 @@ else
             'Body' => array(
                 'Text' => array(
                     // Data is required
-                    'Data' => 'You just registered mah man! Holla back atchyo boi!'
+                    'Data' => 'You just registered mah man! Holla back atchyo boi!\nLogin Information:\nUsername:' . $email . '\nPassword: ' . $pass . '\n'
                     //'Charset' => 'string',
                 )/*
                 'Html' => array(
