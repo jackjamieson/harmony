aa431ddaca935be7bef10917d37a76518923dc36
Jack <jamieson.jack@gmail.com>
2015-04-30 18:33:26 -0400

    set up reading the playlist to connect to db

--- create.php
+++ create.php
@@ -206,7 +206,6 @@ $databaseConnected = $manager->connectToDatabase();
 
                     }
 
-                    close.onclick = reset;
                 </script>';
 
             }
--- template/readPlaylist.php
+++ template/readPlaylist.php
@@ -3,7 +3,18 @@
 if(isset($_GET)){
 
     $roomId = $_GET['id'];
-    readfile('/var/www/html/liq/' . $roomId . '-playlist-full.pls') or die ("Unable to read playlist.");
+    //readfile('/var/www/html/liq/' . $roomId . '-playlist-full.pls') or die ("Unable to read playlist.");
+    
+    $handle = fopen('/var/www/html/liq/' . $roomId . '-playlist-full.pls', "r");
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            echo $line;
+        }
+
+        fclose($handle);
+    } else {
+        // error opening the file.
+    } 
 }
 
 
