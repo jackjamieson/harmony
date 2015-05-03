bbb2476b4968ec4ae998878df3ba1f55a56d52de
Jack <jamieson.jack@gmail.com>
2015-05-03 16:25:29 -0400

    sending user id in readplaylist

--- template/readPlaylist.php
+++ template/readPlaylist.php
@@ -19,7 +19,7 @@ if(isset($_GET)){
 	        $songInfo = $manager->getUserSongFromLocation($lines[0], $lines[1]);
 	       
             echo "<b>" . $songInfo['artist'] . "</b> - " . $songInfo['title'] . "<br>Rating: " . $songInfo['rating'] . "<br><form action='likeSong.php' method='post'><div class='btn-group btn-group-sm' role='group'><button type='submit' name='Up' id='" . $songInfo['song_id'] . "-u' class='btn btn-primary'><span class='glyphicon glyphicon-thumbs-up' aria-hidden='true'></span> Upvote</a><button type='submit' name='Down' id='" . $songInfo['song_id'] . "-d' class='btn btn-primary'><span class='glyphicon glyphicon-thumbs-down' aria-hidden='true'></span> Downvote</a><button type='submit' name='Grab' id='" . $songInfo['song_id'] . "-g' class='btn btn-primary'><span class='glyphicon glyphicon-heart-empty' aria-hidden='true'></span> Grab</a>
-</div><input type='text' name='Artist' class='form-control' value='" . $songInfo['artist'] . "' style='display:none'><input type='text' name='Song' class='form-control' value='" . $songInfo['title'] . "' style='display:none'><input type='text' name='ID' class='form-control' value='" . $songInfo['song_id'] . "' style='display:none'></form><p></p>";
+</div><input type='text' name='Artist' class='form-control' value='" . $songInfo['artist'] . "' style='display:none'><input type='text' name='Song' class='form-control' value='" . $songInfo['title'] . "' style='display:none'><input type='text' name='ID' class='form-control' value='" . $songInfo['song_id'] . "' style='display:none'><input type='text' name='u_id' class='form-control' value='" . $lines[1] . "' style='display:none'></form><p></p>";
         }
         
         
