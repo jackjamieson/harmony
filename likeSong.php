fc9e57153b8a2906f620e4f99b1c28e7269e98c8
root <root@ip-172-31-23-42.ec2.internal>
2015-05-03 20:25:39 +0000

    Altered how like works

--- likeSong.php
+++ likeSong.php
@@ -6,25 +6,25 @@ session_start();
 
 //Start MusicManager to make database queries.
 include "MusicManger.php";
-$manager = new MusicManager();
+$manager = new MusicManager($_SESSION['User_id']);
 $connected = $manager->connectToDatabase();
 
 //Like button pressed
 if(isset($_POST['Up']))
 {
-	$result = $manager->likeSong($_POST['ID'], $_SESSION['User_id']);
+	$result = $manager->likeSong($_POST['ID'], $_POST['u_id']);
 }
 
 //Dislike button pressed
 else if(isset($_POST['Down']))
 {
-	$result = $manager->dislikeSong($_POST['ID'], $_SESSION['User_id']);
+	$result = $manager->dislikeSong($_POST['ID'], $_POST['u_id']);
 }
 
 //Grab button pressed
 else if (isset($_POST['Grab']))
 {
-	$result = $manager->grabSong($_SESSION['User_id'], $_POST['ID'], $_POST['Song'], $_POST['Artist']);
+	$result = $manager->grabSong($_POST['ID'], $_POST['Song'], $_POST['Artist']);
 }
 
 ?>
